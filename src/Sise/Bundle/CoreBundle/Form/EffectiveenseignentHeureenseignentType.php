b81db5b7aed1ca6ddc6591b681d6eb959c7c3dc0
fareh <fareh1986>
2014-12-17 10:06:30 +0100

    CommitRamzi

---
+++ src/Sise/Bundle/CoreBundle/Form/EffectiveenseignentHeureenseignentType.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Sise\Bundle\CoreBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class EffectiveenseignentHeureenseignentType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('codeetab')
+            ->add('codetypeetab')
+            ->add('annescol')
+            ->add('totaheurense')
+            ->add('totaheurensesupp')
+            ->add('totaheurensecomp')
+            ->add('coderece')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Sise\Bundle\CoreBundle\Entity\EffectiveenseignentHeureenseignent'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'sise_corebundle_effectiveenseignentheureenseignent';
+    }
+}
---
+++ src/Sise/Bundle/CoreBundle/Form/EffectiveenseignentListeenseignentTypeenseignementType.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Sise\Bundle\CoreBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class EffectiveenseignentListeenseignentTypeenseignementType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('codeetab')
+            ->add('codetypeetab')
+            ->add('annescol')
+            ->add('idenuniqense')
+            ->add('nompren')
+            ->add('enseangl')
+            ->add('ensetechinfo')
+            ->add('coderece')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Sise\Bundle\CoreBundle\Entity\EffectiveenseignentListeenseignentTypeenseignement'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'sise_corebundle_effectiveenseignentlisteenseignenttypeenseignement';
+    }
+}
