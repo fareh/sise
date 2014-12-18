32c4f19548abe24376057cdd3cc54907f485555d
fareh <fareh.nasreddine@gmail.com>
2014-12-18 17:18:12 +0100

    Nasreddine FAREH : effectifeleve_residantboursier_cycleenseignement  partie

--- src/Sise/Bundle/UserBundle/Controller/DefaultController.php
+++ src/Sise/Bundle/UserBundle/Controller/DefaultController.php
@@ -8,6 +8,9 @@ class DefaultController extends Controller
 {
     public function indexAction($name)
     {
+        if( $this->container->get('security.context')->isGranted('IS_AUTHENTICATED_FULLY') ){
+            return $this->redirect($this->generateUrl('fos_user_security_login'));
+        }
         return $this->render('SiseUserBundle:Default:index.html.twig', array('name' => $name));
     }
 
