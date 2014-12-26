295d7dac848153c23626d99fbd2f7223b0ea6a27
fareh <fareh.nasreddine@gmail.com>
2014-12-26 12:39:36 +0100

    Nasreddine FAREH : effectifeleve_residantboursier_cycleenseignement  partie

--- src/Sise/Bundle/CoreBundle/Controller/InfrastructureEquipementCategorieController.php
+++ src/Sise/Bundle/CoreBundle/Controller/InfrastructureEquipementCategorieController.php
@@ -27,34 +27,37 @@ class InfrastructureEquipementCategorieController extends Controller
      */
     public function editAction(Request $request)
     {
+        $rowspan = array();
         $em = $this->getDoctrine()->getManager();
-        $url = $this->generateUrl('infrastructureequipementcategorie_edit');
-        $search = $this->container->get('form.factory')->createBuilder(new SearchType())->getForm();
         $session = $request->getSession();
-        if ($session->has('features')) {
-            $features = $session->get('features');
-        }
+        $search = $this->container->get('form.factory')->createBuilder(new SearchType($session))->getForm();
         if ($request->isMethod('POST')) {
             $params = $request->request->get($search->getName());
             $session->set("codeetab", $params['NomenclatureEtablissement']);
-            $session->set("features", $params);
             $session->set("codetypeetab", $params['NomenclatureTypeetablissement']);
+            $session->set("features", $params);
+            $search = $this->container->get('form.factory')->createBuilder(new SearchType($session))->getForm();
         }
         $annescol = $session->get('AnneScol');
         $coderece = $session->get('CodeRece');
         $codeetab = ($session->has('codeetab')) ? $session->get('codeetab') : false;
         $codetypeetab = ($session->has('codetypeetab')) ? $session->get('codetypeetab') : false;
+        $url = $this->generateUrl('infrastructureequipementcategorie_edit');
         if ($codeetab && $codetypeetab) {
-            $params = $request->request->get($search->getName());
-            $session->set("features", $params);
-            $entities = $em->getRepository('SiseCoreBundle:InfrastructureEquipementCategorie')->findBy(array('codeetab' => $codeetab, 'codetypeetab' => $codetypeetab));
+            $entities = $em->getRepository('SiseCoreBundle:InfrastructureEquipementCategorie')->getInfrastructureEquipement($codeetab, $codetypeetab, $annescol, $coderece);
+            foreach ($entities as $key => $entity) {
+                $rowspan[$entity->getCodeequi()->getCodecateequi()->getCodecateequi()][$key] = $entity->getCodeequi()->getCodecateequi()->getCodecateequi();
+            }
+            $pathUpdate = $this->generateUrl('infrastructureequipementcategorie_update', array('codeetab' => $codeetab, 'codetypeetab' => $codetypeetab));
         }
         $nameclass = $em->getRepository('SiseCoreBundle:NomenclatureQuestionnaire')->findOneByNameclass('infrastructure_equipement_categorie');
         return $this->render('SiseCoreBundle:Infrastructure:edit.infrastructure_equipement_categorie.html.twig', array(
             'entities' => @$entities,
+            'rowspan' => @$rowspan,
             'search' => $search->createView(),
             'pathfilter' => $url,
-            'nameclass' => $nameclass
+            'nameclass' => $nameclass,
+            'pathUpdate' => @$pathUpdate,
         ));
     }
 
@@ -66,46 +69,41 @@ class InfrastructureEquipementCategorieController extends Controller
     public function updateAction(Request $request, $codeetab, $codetypeetab)
     {
         $em = $this->getDoctrine()->getManager();
-        $url = $this->generateUrl('infrastructureequipementcategorie_edit');
-        $pathUpdate = $this->generateUrl('infrastructureequipementcategorie_update', array('codeetab' => $codeetab, 'codetypeetab' => $codetypeetab));
-        $search = $this->container->get('form.factory')->createBuilder(new SearchType())->getForm();
         $session = $request->getSession();
-        if ($session->has('features')) {
-            $features = $session->get('features');
-        }
+        $search = $this->container->get('form.factory')->createBuilder(new SearchType($session))->getForm();
         $annescol = $session->get('AnneScol');
         $coderece = $session->get('CodeRece');
-        if ($codeetab && $codetypeetab) {
-            $entities = $em->getRepository('SiseCoreBundle:InfrastructureEquipementCategorie')->findBy(array('codeetab' => $codeetab, 'codetypeetab' => $codetypeetab));
+        $entities = $em->getRepository('SiseCoreBundle:InfrastructureEquipementCategorie')->getInfrastructureEquipement($codeetab, $codetypeetab, $annescol, $coderece);
+        foreach ($entities as $key => $entity) {
+            $rowspan[$entity->getCodeequi()->getCodecateequi()->getCodecateequi()][$key] = $entity->getCodeequi()->getCodecateequi()->getCodecateequi();
+        }
+        $url = $this->generateUrl('infrastructureequipementcategorie_edit');
+        $pathUpdate = $this->generateUrl('infrastructureequipementcategorie_update', array('codeetab' => $codeetab, 'codetypeetab' => $codetypeetab));
+        if ($codeetab && $codetypeetab && $request->isMethod('POST')) {
             for ($i = 0; $i < count($entities); $i++) {
-                $items = array_combine(explode("_", $request->request->get('key_' . $i)), explode("_", $request->request->get('val_' . $i)));
-
+                $items = array_combine(explode("|", $request->request->get('key_' . $i)), explode("|", $request->request->get('val_' . $i)));
                 $item = $em->getRepository('SiseCoreBundle:InfrastructureEquipementCategorie')->findOneBy($items);
+                $nombespanonutil = $request->request->get('nombespanonutil' . $i);
+                $nombespautil = $request->request->get('nombespautil' . $i);
+                $obse = $request->request->get('obse' . $i);
 
-                if (!$item) {
-                    throw $this->createNotFoundException('Unable to find SiseCoreBundle entity.');
-                }
-                $subdomains = $item->getCodedoma()->getCodesousdoma();
-                $item->setNombclass($request->request->get('nombclass' . $i));
-                $item->setNombelevmasc($request->request->get('nombelevmasc' . $i));
-                $item->setNombelevfemi($request->request->get('nombelevfemi' . $i));
-                $item->setNombtotaelev($request->request->get('nombtotaelev' . $i));
+                $item->setNombespanonutil($nombespanonutil);
+                $item->setNombespautil($nombespautil);
+                $item->setObse($obse);
                 $em->persist($item);
-                foreach ($subdomains as $subdomain) {
-                    $subdomain->setOrdraffi($request->request->get('codesousdoma_ordraffi_' . $subdomain->getCodesousdoma() . '_' . $i));
-                    $em->persist($subdomain);
-                }
-
                 $em->flush();
             }
+            return $this->redirect($this->generateUrl('infrastructureequipementcategorie_edit'));
         }
         $nameclass = $em->getRepository('SiseCoreBundle:NomenclatureQuestionnaire')->findOneByNameclass('infrastructure_equipement_categorie');
         return $this->render('SiseCoreBundle:Infrastructure:edit.infrastructure_equipement_categorie.html.twig', array(
             'entities' => @$entities,
+            'rowspan' => @$rowspan,
             'search' => $search->createView(),
             'pathfilter' => $url,
+            'nameclass' => $nameclass,
             'pathUpdate' => @$pathUpdate,
-            'nameclass' => $nameclass
+
         ));
     }
 
@@ -119,23 +117,21 @@ class InfrastructureEquipementCategorieController extends Controller
         $rowspan = array();
         $em = $this->getDoctrine()->getManager();
         $url = $this->generateUrl('infrastructureequipementcategorie_list');
-        $search = $this->container->get('form.factory')->createBuilder(new SearchType())->getForm();
         $session = $request->getSession();
+        $search = $this->container->get('form.factory')->createBuilder(new SearchType($session))->getForm();
         if ($request->isMethod('POST')) {
             $params = $request->request->get($search->getName());
             $session->set("codeetab", $params['NomenclatureEtablissement']);
             $session->set("codetypeetab", $params['NomenclatureTypeetablissement']);
+            $session->set("features", $params);
+            $search = $this->container->get('form.factory')->createBuilder(new SearchType($session))->getForm();
         }
         $annescol = $session->get('AnneScol');
         $coderece = $session->get('CodeRece');
         $codeetab = ($session->has('codeetab')) ? $session->get('codeetab') : false;
         $codetypeetab = ($session->has('codetypeetab')) ? $session->get('codetypeetab') : false;
         if ($codeetab && $codetypeetab) {
-            //     $entities = $em->getRepository('SiseCoreBundle:InfrastructureEquipementCategorie')->findBy(array('codeetab' => $codeetab, 'codetypeetab' => $codetypeetab, 'annescol'=>$annescol, 'coderece'=>$coderece));
-
             $entities = $em->getRepository('SiseCoreBundle:InfrastructureEquipementCategorie')->getInfrastructureEquipement($codeetab, $codetypeetab, $annescol, $coderece);
-
-
             foreach ($entities as $key => $entity) {
                 $rowspan[$entity->getCodeequi()->getCodecateequi()->getCodecateequi()][$key] = $entity->getCodeequi()->getCodecateequi()->getCodecateequi();
             }
