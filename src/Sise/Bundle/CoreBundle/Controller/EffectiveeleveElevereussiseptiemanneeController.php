98451b9db763056db277ef974d80ccc27e16f452
fareh <fareh.nasreddine@gmail.com>
2014-12-30 10:31:17 +0100

    Nasreddine FAREH : effectifeleve_residantboursier_cycleenseignement  partie

---
+++ src/Sise/Bundle/CoreBundle/Controller/EffectiveeleveElevereussiseptiemanneeController.php
@@ -0,0 +1,126 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hp
+ * Date: 12/12/2014
+ * Time: 14:43
+ */
+
+namespace Sise\Bundle\CoreBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Sise\Bundle\CoreBundle\Entity\EffectiveeleveElevereussiseptiemannee;
+use Sise\Bundle\CoreBundle\Form\search\SearchType;
+
+/**
+ * EffectiveeleveElevereussiseptiemannee controller.
+ *
+ */
+class EffectiveeleveElevereussiseptiemanneeController extends Controller
+{
+
+
+    /**
+     * Displays a form to edit an existing NomenclatureQuestionnaire entity.
+     *
+     */
+    public function editAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $session = $request->getSession();
+        $search = $this->container->get('form.factory')->createBuilder(new SearchType($session))->getForm();
+        if ($request->isMethod('POST')) {
+            $params = $request->request->get($search->getName());
+            $session->set("codeetab", $params['NomenclatureEtablissement']);
+            $session->set("codetypeetab", $params['NomenclatureTypeetablissement']);
+            $session->set("features", $params);
+            $search = $this->container->get('form.factory')->createBuilder(new SearchType($session))->getForm();
+        }
+        $annescol = $session->get('AnneScol');
+        $coderece = $session->get('CodeRece');
+        $codeetab = ($session->has('codeetab')) ? $session->get('codeetab') : false;
+        $codetypeetab = ($session->has('codetypeetab')) ? $session->get('codetypeetab') : false;
+        $url = $this->generateUrl('effectiveeleveelevereussiseptiemannee_edit');
+        $pathUpdate = $this->generateUrl('effectiveeleveelevereussiseptiemannee_update', array('codeetab' => $codeetab, 'codetypeetab' => $codetypeetab));
+        if ($codeetab && $codetypeetab) {
+            $entity = $em->getRepository('SiseCoreBundle:EffectiveeleveElevereussiseptiemannee')->findOneBy(array('codeetab' => $codeetab, 'codetypeetab' => $codetypeetab, 'annescol' => $annescol, 'coderece' => $coderece));
+        }
+        $nameclass = $em->getRepository('SiseCoreBundle:NomenclatureQuestionnaire')->findOneByNameclass('effectiveeleve_elevereussiseptiemannee');
+        return $this->render('SiseCoreBundle:NomenclatureQuestionnaire:edit.effectiveeleve_elevereussiseptiemannee.html.twig', array(
+            'entity' => @$entity,
+            'search' => $search->createView(),
+            'pathfilter' => $url,
+            'pathUpdate' => @$pathUpdate,
+            'nameclass' => $nameclass
+        ));
+    }
+
+
+    /**
+     * Edits an existing NomenclatureQuestionnaire entity.
+     *
+     */
+    public function updateAction(Request $request, $codeetab, $codetypeetab)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $session = $request->getSession();
+        $search = $this->container->get('form.factory')->createBuilder(new SearchType($session))->getForm();
+        $annescol = $session->get('AnneScol');
+        $coderece = $session->get('CodeRece');
+        $url = $this->generateUrl('effectiveeleveelevereussiseptiemannee_edit');
+        $pathUpdate = $this->generateUrl('effectiveeleveelevereussiseptiemannee_update', array('codeetab' => $codeetab, 'codetypeetab' => $codetypeetab));
+        $entity = $em->getRepository('SiseCoreBundle:EffectiveeleveElevereussiseptiemannee')->findOneBy(array('codeetab' => $codeetab, 'codetypeetab' => $codetypeetab, 'annescol' => $annescol, 'coderece' => $coderece));
+        if ($codeetab && $codetypeetab && $request->isMethod('POST')) {
+            $entity->setNombelevmasc($request->request->get('nombelevmasc'));
+            $entity->setNombelevfemi($request->request->get('nombelevfemi'));
+            $entity->setNombtotaelev($request->request->get('nombelevmasc') + $request->request->get('nombelevfemi'));
+            $em->persist($entity);
+            $em->flush();
+            return $this->redirect($this->generateUrl('effectiveeleveelevereussiseptiemannee_edit'));
+        }
+        $nameclass = $em->getRepository('SiseCoreBundle:NomenclatureQuestionnaire')->findOneByNameclass('effectiveeleve_elevereussiseptiemannee');
+        return $this->render('SiseCoreBundle:NomenclatureQuestionnaire:edit.effectiveeleve_elevereussiseptiemannee.html.twig', array(
+            'entity' => @$entity,
+            'search' => $search->createView(),
+            'pathfilter' => $url,
+            'pathUpdate' => @$pathUpdate,
+            'nameclass' => $nameclass
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing NomenclatureQuestionnaire entity.
+     *
+     */
+    public function listAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $url = $this->generateUrl('effectiveeleveelevereussiseptiemannee_list');
+        $session = $request->getSession();
+        $search = $this->container->get('form.factory')->createBuilder(new SearchType($session))->getForm();
+        if ($request->isMethod('POST')) {
+            $params = $request->request->get($search->getName());
+            $session->set("codeetab", $params['NomenclatureEtablissement']);
+            $session->set("codetypeetab", $params['NomenclatureTypeetablissement']);
+            $session->set("features", $params);
+            $search = $this->container->get('form.factory')->createBuilder(new SearchType($session))->getForm();
+        }
+        $annescol = $session->get('AnneScol');
+        $coderece = $session->get('CodeRece');
+        $codeetab = ($session->has('codeetab')) ? $session->get('codeetab') : false;
+        $codetypeetab = ($session->has('codetypeetab')) ? $session->get('codetypeetab') : false;
+        if ($codeetab && $codetypeetab) {
+            $entities = $em->getRepository('SiseCoreBundle:EffectiveeleveElevereussiseptiemannee')->findBy(array('codeetab' => $codeetab, 'codetypeetab' => $codetypeetab, 'annescol' => $annescol, 'coderece' => $coderece));
+        }
+        $nameclass = $em->getRepository('SiseCoreBundle:NomenclatureQuestionnaire')->findOneByNameclass('effectiveeleve_elevereussiseptiemannee');
+        return $this->render('SiseCoreBundle:NomenclatureQuestionnaire:list.effectiveeleve_elevereussiseptiemannee.html.twig', array(
+            'entities' => @$entities,
+            'search' => $search->createView(),
+            'pathfilter' => $url,
+            'nameclass' => $nameclass
+        ));
+    }
+
+} 
