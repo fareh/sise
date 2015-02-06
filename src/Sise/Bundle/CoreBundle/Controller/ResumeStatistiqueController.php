eb3efd5bafd7a0cba9fe1f8e09f56d8b5dbe94b7
fareh <fareh1986>
2015-02-06 17:08:59 +0100

    CommitRamzi

---
+++ src/Sise/Bundle/CoreBundle/Controller/ResumeStatistiqueController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Sise\Bundle\CoreBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Config\Loader;
+
+class ResumeStatistiqueController extends Controller
+{
+    public function storedProcedure($proc_name,$codecyclensei){
+        $em = $this->getDoctrine()->getManager()->getConnection();
+       // $sth = $em->prepare("CALL ".$proc_name."(".$codecyclensei.");");
+        $resultat = $em->query("CALL ".$proc_name."(".$codecyclensei.");")->fetchAll();
+      //  $sth->execute();
+        return $resultat;
+    }
+    public function listresustatAction(Request $request)
+    {
+        $entities=$this->storedProcedure('SP_ResuStat_ResumeStatistiquePrep','0');
+        foreach($entities as $key=>$en)
+        {
+
+        }
+        return $this->render('SiseCoreBundle:ResumeStatistique:listresustat.html.twig', array('entities' => $entities));
+    }
+}
